d84467c347d49e32cf33afe5628462bf39281819
Shepovalov-k <145323490+Shepovalov-k@users.noreply.github.com>
2024-01-05 14:40:41 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3.9
 
+import argparse
+import numpy as np
+
+
+
+def read_xyz(fname):
+    return np.genfromtxt(fname, skip_header=2,
+               names = ['atom','x', 'y', 'z'],
+               dtype = "S5,f4,f4,f4")
+
+def read_gro(fname):
+    with open(fname) as f:
+        *_, last_line = f.readlines()
+        del _
+    vector = np.fromstring(last_line,
+                 dtype=float,
+                 sep=' ')
+    return np.genfromtxt(fname, skip_header=2, skip_footer=1,
+               names = ['mid', 'mol', 'atom', 'id', 'x', 'y', 'z'],
+               dtype = "i4,S5,S6,i4,f4,f4,f4",
+               invalid_raise=True, autostrip=True,
+               delimiter=[5,5,5,5,8,8,8]), vector
 
 
 
 def main():
-    pass
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, help="the name of xyz-file")
+    args = parser.parse_args()
+    print(read_gro(args.filename))
 
 
 if __name__ == "__main__":
     main()
-
